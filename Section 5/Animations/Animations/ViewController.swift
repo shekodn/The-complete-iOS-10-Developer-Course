9a2910f6644319f2f4111dbf6c9dac17510eace7
shekodn <sdiazn19@gmail.com>
2016-12-12 14:44:08 -0600

    adds fade, slide, grow

--- Section 5/Animations/Animations/ViewController.swift
+++ Section 5/Animations/Animations/ViewController.swift
@@ -11,11 +11,39 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var startLabel: UIButton!
+    
+    @IBAction func fadeIn(_ sender: Any) {
+        image.alpha = 0
+        UIView.animate(withDuration: 1, animations: {
+            self.image.alpha = 1
+        })
+    }
+    
+    @IBAction func grow(_ sender: Any) {
+        image.frame = CGRect(x: 0, y:0 , width:0 , height:0)
+        
+        UIView.animate(withDuration:1){
+            self.image.frame = CGRect(x: 0, y:0 , width:200 , height:200)
+
+        }
+    }
+    
+    @IBAction func slideIn(_ sender: Any) {
+        
+        image.center = CGPoint(x:image.center.x - 500 , y: image.center.y )
+        
+        UIView.animate(withDuration:0.2){
+            self.image.center = CGPoint(x:self.image.center.x + 500 , y: self.image.center.y)
+        }
+    }
+
+    var timer = Timer()
+    var isAnimating = false
     var iCont = 0;
 
     
-    
-    @IBAction func next(_ sender: Any) {
+    func animate(){
         
         iCont+=1;
         
@@ -23,10 +51,24 @@ class ViewController: UIViewController {
             iCont = 0
         }
         
-        
         image.image = UIImage(named: "frame_" + String(iCont) + "_delay-0.08s.gif")
+    }
+    
+    @IBAction func next(_ sender: Any) {
         
-        print(iCont)
+        isAnimating = !isAnimating
+        
+        if(isAnimating){
+            
+            startLabel.setTitle("Stop", for: .normal)
+
+            
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.animate) , userInfo: nil, repeats: true)
+        } else{
+            startLabel.setTitle("Start", for: .normal)
+            timer.invalidate()
+        }
+
     }
     
     
