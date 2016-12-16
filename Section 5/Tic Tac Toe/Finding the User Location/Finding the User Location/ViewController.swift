0c037d88b5013c88b7c9d34dc732f66566c313f1
shekodn <sdiazn19@gmail.com>
2016-12-15 20:17:02 -0600

    adds user location map template

---
+++ Section 5/Tic Tac Toe/Finding the User Location/Finding the User Location/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Finding the User Location
+//
+//  Created by Sergio Díaz Navarro on 12/15/16.
+//  Copyright © 2016 Sergio Díaz Navarro. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
+    
+    var locationManager = CLLocationManager()
+    @IBOutlet weak var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        
+        let userLocation: CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+
+        self.map.setRegion(region, animated: true)
+        
+
+        
+    }
+
+
+}
+
