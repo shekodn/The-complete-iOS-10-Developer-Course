50d7703dcd292a788e14184c6f040b4d2e231602
shekodn <sdiazn19@gmail.com>
2016-12-26 22:30:34 -0600

    adds code for swipes and shakes

---
+++ Section 5/Swipes & Shakes/Swipes & Shakes/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Swipes & Shakes
+//
+//  Created by Rob Percival on 21/06/2016.
+//  Copyright © 2016 Appfish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+            
+            print("Device was shaken")
+            
+        }
+        
+    }
+    
+    func swiped(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+                
+            case UISwipeGestureRecognizerDirection.right:
+                print("User Swiped Right")
+            case UISwipeGestureRecognizerDirection.left:
+                print("User Swiped Left")
+            default:
+                break
+                
+            }
+            
+            
+        }
+        
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
