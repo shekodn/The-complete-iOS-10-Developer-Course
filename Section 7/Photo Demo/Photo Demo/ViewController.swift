535370414743df7445209853470d735b0f1ad5dc
shekodn <sdiazn19@gmail.com>
2017-01-09 14:40:25 -0600

    Finishes photo demo app

--- Section 7/Photo Demo/Photo Demo/ViewController.swift
+++ Section 7/Photo Demo/Photo Demo/ViewController.swift
@@ -8,8 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
+    @IBOutlet weak var imageView: UIImageView!
+
+    
+    
+    @IBAction func importPhoto(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.delegate = self
+        
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        imagePickerController.allowsEditing =  false //image comes directly
+        
+        self.present(imagePickerController, animated: true, completion: nil)
+        
+        
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            
+            imageView.image = image
+        } else{
+            
+            print("Error. Problem Getting the Image")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
